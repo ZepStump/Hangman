880e92b531bf59d63fd5aa8636a81ceeea642ded
sherry122232 <1006370919@qq.com>
2023-04-26 11:47:55 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Leaderboard.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Hangman from "./Hangman";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
     // implementing routes to later handle custom game url
     <Routes>
       <Route path="*" element={<Hangman />} />
+      <Route path="/Leaderboard" element={<Leaderboard />} />
     </Routes>
   );
 }
