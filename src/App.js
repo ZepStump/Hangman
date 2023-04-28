3414ccfe827dae8e4e15da6dbef7dc970136774b
BertMorris <bertmorris911@gmail.com>
2023-04-28 14:32:29 -0700

    trying hash router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Hangman from "./Hangman";
-import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
     // implementing routes to later handle custom game url
     <Routes>
+      <Route path="/:customWord?" element={<Hangman />} />
       <Route path="Hangman/:customWord?" element={<Hangman />} />
-      <Route path="/Leaderboard" element={<Leaderboard />} />
     </Routes>
   );
 }
